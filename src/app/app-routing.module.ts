8242a0c3b052d591d77af5cd199d2ece12db4be2
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-07-24 18:29:14 +0100

    aplicando css base nos components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { NewPostComponent } from './components/pages/new-post/new-post.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { NgModule } from '@angular/core';
@@ -6,6 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'posts/new', component: NewPostComponent },
 ];
 
 @NgModule({
